6d420fa1eca3f4dad5987ea40ceefc72b0697b73
Nouwatin Jacob <jaysansa@gmail.com>
2018-04-23 23:38:26 +0100

    feature(get-all-meals): -check the array if is not empty -get all the meals options in the array

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,13 @@
+import MealsController from '../controllers/meals';
 
 const routes = (router) => {
+  const mealCtrl = new MealsController(router);
   router.route('/')
     .get((req, res) => res.status(200).json({
       message: 'Welcome to the More Book-A-Meal API!',
     }));
+
+  router.route('/meals').get(mealCtrl.getMeals);
 };
 
 export default routes;
