cca61a75866a5856f268a046111d72c7484080b2
Nouwatin Jacob <jaysansa@gmail.com>
2018-04-26 12:00:12 +0100

    created helper file for meal test

---
+++ server/test/helper/meals.js
@@ -0,0 +1,21 @@
+const meals = [{
+  id: 1,
+  name: 'Fried Rice and Chicken',
+  price: 1500,
+  image: 'friedrice.jpg',
+},
+{
+  id: 2,
+  name: 'Eba and Egusi Soup',
+  price: 2000,
+  image: 'ebaandegusi.png',
+},
+{
+  id: 3,
+  name: 'Ofada Rice with assorted Stew',
+  price: 2500,
+  image: 'ofadarice.jpeg',
+},
+];
+
+export default meals;
