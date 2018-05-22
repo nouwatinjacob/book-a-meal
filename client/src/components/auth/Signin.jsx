6414713fe93767b134b8fe3210da84e90efb439d
Nouwatin Jacob <jaysansa@gmail.com>
2018-05-23 00:27:07 +0100

    create Login component

--- client/src/components/auth/Signin.jsx
+++ client/src/components/auth/Signin.jsx
@@ -1,15 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../partials/Header.jsx';
 
 /**
  * Login class declaration
  *
- * @class Homepage
+ * @class Login
  *
  * @extends {React.Component}
  */
-class Login extends React.Component {
+class Login extends Component {
+  state = {
+    loginData: {
+      email: '',
+      password: ''
+    },
+    error: undefined
+  };
+
+  /**
+  * Handle Login input change
+  *
+  * @param {event} event
+  *
+  * @return {event} event
+  *
+  */
+  onInputChange = (event) => {
+    const { email, password } = this.state.loginData;
+    const loginData = { email, password };
+    loginData[event.target.name] = event.target.value;
+    this.setState(() => ({ loginData }));
+  };
+
+  /**
+  * Handle submit
+  *
+  * @param {event} event
+  *
+  * @return {event} event
+  *
+  */
+  onSubmit = (event) => {
+    event.preventDefault();
+    if (!this.state.loginData.email || !this.state.loginData.password) {
+      this.setState(() => ({ error: 'Please provide email and password.' }));
+    } else {
+      this.setState(() => ({ error: '' }));
+      console.log(this.state.loginData);
+    }
+  };
+
+  /**
+    * Renders Login component
+    *
+    * @returns {XML} XML/JSX
+*/
   render() {
     return (
       <div className='container'>
@@ -19,10 +65,23 @@ class Login extends React.Component {
           <div className='login'>
             <div className='login-form'>
               <h3>User Login</h3>
-              <form>
-                <input type='text' placeholder='Email' required/><br/>
-                <input type='password' placeholder='Password' required/><br/>
-                <button className='button warning'><a href='caterer.html'>Login</a></button>
+              <form onSubmit={this.onSubmit}>
+              {this.state.error && <p className="form-error">{this.state.error}</p>}
+                <input
+                  type='text'
+                  name='email'
+                  placeholder='Email'
+                  value={this.state.loginData.email}
+                  onChange={this.onInputChange}
+                /><br/>
+                <input
+                  type='password'
+                  name='password'
+                  placeholder='Password'
+                  value={this.state.loginData.password}
+                  onChange={this.onInputChange}
+                /><br/>
+                <button className='button warning'>Login</button>
                 <p>Forgot <a href='#'>Password?</a></p>
               </form>
             </div>
